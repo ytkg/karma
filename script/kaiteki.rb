db03671c24e2132e4c271756cc952db783f6f980
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2025-08-01 23:34:47 +0900

    リファクタ

--- script/kaiteki.rb
+++ script/kaiteki.rb
@@ -2,34 +2,73 @@ require 'hitoku'
 require 'switchbot'
 require 'ruby-ambient'
 
-client = Switchbot::Client.new(Hitoku.switchbot_api_token, Hitoku.switchbot_api_secret)
+class Kaiteki
+  TARGET_TEMPERATURE = 24
 
-device = client.device('B0E9FE5580EE')
-status = device.status
+  def execute
+    current_metrics = fetch_current_metrics
 
-temperature = status[:body][:temperature]
-p "Current temperature: #{temperature}°C"
+    current_metrics.each do |name, value|
+      p "Current #{name}: #{value}"
+    end
 
-humidity = status[:body][:humidity]
-p "Current humidity: #{humidity}%"
+    previous_metrics = fetch_previous_metrics
 
-discomfort_index = 0.81 * temperature + humidity * 0.01 * (0.99 * temperature - 14.3) + 46.3
-p "Current discomfort_index: #{discomfort_index}"
+    aircon = switchbot_client.device('02-202103110155-72537419')
+    set_temperature = previous_metrics[:set_temperature]
 
-am = Ambient.new('93486', write_key: Hitoku.ambient_write_key, read_key: Hitoku.ambient_read_key)
-last_data = am.read().last
+    if current_metrics[:temperature] >= TARGET_TEMPERATURE + 2
+      set_temperature -= 1
+      aircon.commands(command: 'setAll', parameter: "#{set_temperature},2,1,on", command_type: 'command')
+    end
 
-aircon = client.device('02-202103110155-72537419')
-set_temperature = last_data[:d4]
+    if current_metrics[:temperature] <= TARGET_TEMPERATURE - 2
+      set_temperature += 1
+      aircon.commands(command: 'setAll', parameter: "#{set_temperature},2,1,on", command_type: 'command')
+    end
 
-if temperature >= 24.2
-  set_temperature -= 1
-  aircon.commands(command: 'setAll', parameter: "#{set_temperature},2,1,on", command_type: 'command')
+    send_metrics(current_metrics.merge(set_temperature:))
+  end
+
+  private
+
+  def switchbot_client
+    @switchbot_client ||= Switchbot::Client.new(Hitoku.switchbot_api_token, Hitoku.switchbot_api_secret)
+  end
+
+  def ambient_client
+    @ambient_client ||= Ambient.new('93486', write_key: Hitoku.ambient_write_key, read_key: Hitoku.ambient_read_key)
+  end
+
+  def calculate_discomfort_index(temperature, humidity)
+    0.81 * temperature + humidity * 0.01 * (0.99 * temperature - 14.3) + 46.3
+  end
+
+  def fetch_current_metrics
+    device = switchbot_client.device('B0E9FE5580EE')
+    status = device.status
+
+    temperature = status[:body][:temperature]
+    humidity = status[:body][:humidity]
+    discomfort_index = calculate_discomfort_index(temperature, humidity)
+
+    { temperature:, humidity:, discomfort_index: }
+  end
+
+  def fetch_previous_metrics
+    metrics = ambient_client.read.last
+
+    {
+      temperature: metrics[:d1],
+      humidity: metrics[:d2],
+      discomfort_index: metrics[:d3],
+      set_temperature: metrics[:d4]
+    }
+  end
+
+  def send_metrics(metrics)
+    ambient_client.send(d1: metrics[:temperature], d2: metrics[:humidity], d3: metrics[:discomfort_index], d4: metrics[:set_temperature])
+  end
 end
 
-if temperature <= 23.8
-  set_temperature += 1
-  aircon.commands(command: 'setAll', parameter: "#{set_temperature},2,1,on", command_type: 'command')
-end
-
-am.send(d1: temperature, d2: humidity, d3: discomfort_index, d4: set_temperature)
+Kaiteki.new.execute
