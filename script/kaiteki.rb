2f74e32d5bb0bad21ed448ca48a4bd67dd9ca67a
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2025-08-01 21:06:02 +0900

    feat: 前回の設定温度から今回の設定温度を決めるようにした

--- script/kaiteki.rb
+++ script/kaiteki.rb
@@ -16,18 +16,20 @@ p "Current humidity: #{humidity}%"
 discomfort_index = 0.81 * temperature + humidity * 0.01 * (0.99 * temperature - 14.3) + 46.3
 p "Current discomfort_index: #{discomfort_index}"
 
-aircon = client.device('02-202103110155-72537419')
+am = Ambient.new('93486', write_key: Hitoku.ambient_write_key, read_key: Hitoku.ambient_read_key)
+last_data = am.read().last
 
-set_temperature = 0
+aircon = client.device('02-202103110155-72537419')
+set_temperature = last_data[:d4]
+
 if temperature >= 24.3
-  set_temperature = 27
+  set_temperature -= 1
   aircon.commands(command: 'setAll', parameter: "#{set_temperature},2,1,on", command_type: 'command')
 end
 
 if temperature <= 23.7
-  set_temperature = 29
+  set_temperature += 1
   aircon.commands(command: 'setAll', parameter: "#{set_temperature},2,1,on", command_type: 'command')
 end
 
-am = Ambient.new('93486', write_key: Hitoku.ambient_write_key)
 am.send(d1: temperature, d2: humidity, d3: discomfort_index, d4: set_temperature)
